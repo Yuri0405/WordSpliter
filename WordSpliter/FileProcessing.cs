86adaa25fbf9beae3d5630fbba4fd8e4caecaa42
Yuri0405 <60585835+Yuri0405@users.noreply.github.com>
2021-12-11 02:47:01 +0200

    Update FileProcessing.cs

--- WordSpliter/FileProcessing.cs
+++ WordSpliter/FileProcessing.cs
@@ -22,33 +22,6 @@ namespace WordSpliter
             }
             return collection;
         }
-
-
-        /*
-        public static void ReadFromFile( string filePath, HashSet<string> dictionary)
-        {
-            using(StreamReader sr = new StreamReader(filePath))
-            {
-                string word;
-                while ((word = sr.ReadLine()) != null)
-                {
-                    dictionary.Add(word.ToLower());
-                }
-            }   
-        }
-
-        public static void ReadFromFile(string filePath, List<string> testWords)
-        {
-            using (StreamReader sr = new StreamReader(filePath))
-            {
-                string word;
-                while ((word = sr.ReadLine()) != null)
-                {
-                    testWords.Add(word.ToLower());
-                }
-            }
-
-        }*/
     }
 }
 
